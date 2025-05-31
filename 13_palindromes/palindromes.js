b5b534976434cd9795654d5efa0d722faa405e98
abramepithey <abramepithey@gmail.com>
2025-05-30 22:44:40 -0600

    Solves Palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    const allowedCharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let fixedString = inputString.toLowerCase().split("").filter(character => allowedCharacters.includes(character)).join("");
+    return [...fixedString].reverse().join("") === fixedString;
 };
 
 // Do not edit below this line
