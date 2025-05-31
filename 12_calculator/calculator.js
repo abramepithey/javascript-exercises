6d9dc5facbfece164ec712357e93aaa71a8c7ee1
abramepithey <abramepithey@gmail.com>
2025-05-30 21:35:35 -0600

    My solution to the Calculator problem

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return Number(num1) + Number(num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+	return numList.reduce((ongoingTotal, currentNum) => {
+    return ongoingTotal += currentNum;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  return numList.reduce((ongoingTotal, currentNum) => {
+    return ongoingTotal * currentNum;
+  })
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(inputNum) {
+	let ongoingNum = 1;
+  for (let i = inputNum; i > 0; i--) {
+    ongoingNum = ongoingNum * i;
+  }
+  return ongoingNum;
 };
 
 // Do not edit below this line
