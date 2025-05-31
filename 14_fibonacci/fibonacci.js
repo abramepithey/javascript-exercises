c01c723fb72f04e2963ed1a2dc241e6ba43350a0
abramepithey <abramepithey@gmail.com>
2025-05-31 14:24:59 -0600

    Solves the Fibonacci problem

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(distance) {
+    if (Number(distance) < 0 || isNaN(Number(distance)))
+        return "OOPS";
+    let first = 0;
+    let second = 1;
+    for (let i = 1; i <= Number(distance); i++) {
+        let interim = first + second;
+        first = second;
+        second = interim;
+    }
+    return first;
 };
 
 // Do not edit below this line
