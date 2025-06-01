d7339c46a055556286f6deb2ddbe5156414927e5
abramepithey <abramepithey@gmail.com>
2025-06-01 12:27:00 -0600

    First solution to findTheOldest problem

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson;
+    let oldestAge = 0;
+    people.forEach(person => {
+        let birthDate = new Date(person.yearOfBirth + "-1-1");
+        let fullAge;
+        if (Object.hasOwn(person, "yearOfDeath")) {
+            fullAge = new Date(person.yearOfDeath + "-1-1") - birthDate;
+        } else {
+            fullAge = new Date(Date.now()) - birthDate;
+        }
 
+        if (oldestPerson === undefined || fullAge > oldestAge) {
+            oldestPerson = person;
+            oldestAge = fullAge;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
